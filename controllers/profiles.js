4599ce19931e3ea5b3f7c878f4366cb7bbb120ac
emma <emma.ronaldo7@gmail.com>
2022-04-28 11:30:22 -0400

    create loveArmy

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -9,8 +9,20 @@ function index(req, res) {
   })
 }
 
-function create(req, res){
-  
+const create = async(req, res) => {
+  try {
+    const profile= await Profile.findById(req.user.profile)
+    profile.loveArmy.push(req.body)
+    console.log("req", profile);
+    await profile.save()
+    const newSoldier = profile.loveArmy[profile.loveArmy.length - 1];
+		return res.status(201).json(newSoldier);
+  } catch (error) {
+    throw error
+  }
 }
 
-export { index }
+export {
+  index,
+  create
+  }
