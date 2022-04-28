b04794574a6e9251a9cf474cb1daced2cbb77d2c
Payal Desai <payal@idesai.com>
2022-04-28 09:49:23 -0400

    update models

--- models/profile.js
+++ models/profile.js
@@ -1,11 +1,64 @@
 import mongoose from 'mongoose'
 
-const profileSchema = new mongoose.Schema({
+const Schema = mongoose.Schema
+
+const giftSchema = new Schema ({
+  name: {
+    type: String,
+    required: true
+  },
+  where: String,
+  note: String
+})
+
+const reminderSchema = new Schema ({
+  name: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String
+  }
+})
+
+const loveArmySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  totalPoints: {
+    type: Number,
+    default: 250,
+  },
+  currentPoints: {
+    type: Number,
+    default: 125,
+  },
+  gifts: [{
+    giftSchema
+  }],
+  reminders: [{
+    reminderSchema
+  }],
+  plans: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Plan"
+  }],
+  status: {
+    type: Boolean,
+    default: true,
+  }
+})
+
+const profileSchema = new Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
   loveArmy: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "LoveArmy"
+    loveArmySchema
   }],
   plans: [{
     type: mongoose.Schema.Types.ObjectId,
