5bade6155bca4b6662948227319edf25a0520bf7
Emily Lascano <91232529+e-lascano2021@users.noreply.github.com>
2022-04-28 20:00:41 -0400

    Update LoveArmyHeader.jsx

--- src/components/LoveArmy/LoveArmyHeader.jsx
+++ src/components/LoveArmy/LoveArmyHeader.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom"
 
 const LoveArmyHeader = () => {
   const navigate = useNavigate()
+  
   const handleAddSoldier = () => {
     navigate("/loveArmy/new")
   }
