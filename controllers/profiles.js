cb19bed637671dfc88a07abd6873348f62f4df17
e.lascano <emilas5678@gmail.com>
2022-04-29 12:10:50 -0400

    create route and controller for myProfile

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,5 +1,15 @@
 import { Profile } from '../models/profile.js'
 
+
+const show = async (req, res) => {
+  try {
+    const profile = await Profile.findById(req.params.id);
+    return res.status(201).json(profile);
+  } catch (error) {
+    throw error
+  }
+}
+
 const index = async(req, res) => {
   try {
     const profile = await Profile.findById(req.user.profile);
@@ -22,6 +32,7 @@ const create = async(req, res) => {
 }
 
 export {
+  show,
   index,
   create
-  }
+}
