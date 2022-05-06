f204f9a41c32580256b19e0b53303b32df097780
e.lascano <emilas5678@gmail.com>
2022-05-06 14:10:22 -0400

    complete grid on landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Login from '../../components/Login/Login.jsx'
 import Signup from '../../components/Signup/Signup.jsx'
-// import styles from '../../assets/cssPages/Landing'
+import styles from '../../assets/cssPages/Landing.module.css'
 
 const Landing = (props) => {
   const [form, setForm]= useState("none")
@@ -11,23 +11,25 @@ const Landing = (props) => {
   }
 
   return (
-    <main>
-      <div>
-        <h6>Love You</h6>
-        <h6>More</h6>
+    <main className={styles.container}>
+
+      <div className={styles.top}>
+        <h1>Love You</h1>
+        <h1>More</h1>
         <p onClick={() => handleForm("LogIn")}>LogIn</p>
         <p onClick={() => handleForm("SignUp")}>Signup</p>
       </div>
-
-      { form === "LogIn" ?
-        <Login {...props}/>
-      : form === "SignUp" ?
-        <Signup {...props}/> 
-      :
-        <div>
+      
+      <div className={styles.bottom}>
+        { form === "LogIn" ?
+          <Login {...props}/>
+          : form === "SignUp" ?
+          <Signup {...props}/> 
+          :
           <h4>about the app</h4>
-        </div>
-      }
+        }
+      </div>
+
     </main>
   )
 }
