8969284ebabe82759025f6732dd6a568170d0241
e.lascano <emilas5678@gmail.com>
2022-07-15 15:07:12 -0400

    display love languages on soldier page

--- src/components/Soldier/SoldierHeader.jsx
+++ src/components/Soldier/SoldierHeader.jsx
@@ -5,12 +5,14 @@ const SoldierHeader = (props) =>{
   return (
     <>
       <h1>{props.soldier.name}</h1>
-      <label>LoveTypes: </label>
+      <label>Love Types: </label>
       <h4>{props.soldier.loveTypes?.join(", ")}</h4>
+      <label>Love Languages: </label>
+      <h4>{props.soldier.loveLanguages?.join(", ")}</h4>
       <label>
         <h4>Brownie Points:</h4>
-        <BrowniePoints soldier={props.soldier}/>
       </label>
+        <BrowniePoints soldier={props.soldier}/>
     </>
   )
 }
