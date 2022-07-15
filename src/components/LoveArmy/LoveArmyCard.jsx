40ade79027a382db0eccdab1cb96caa69cc73d7e
e.lascano <emilas5678@gmail.com>
2022-07-15 09:34:28 -0400

    display LoveTypes of each soldier on LoveArmy page

--- src/components/LoveArmy/LoveArmyCard.jsx
+++ src/components/LoveArmy/LoveArmyCard.jsx
@@ -4,6 +4,8 @@ const LoveArmyCard = (props) => {
   return (
     <>
       <h1>{props.cardInfo.name}</h1>
+      <label>LoveTypes: </label>
+      <h4>{props.cardInfo.loveTypes.join(", ")}</h4>
       <BrowniePoints soldier={props.cardInfo}/>
     </>
   )
