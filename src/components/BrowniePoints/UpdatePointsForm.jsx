f202a083cb7df5b687f6dec394b68586403c3dc0
e.lascano <emilas5678@gmail.com>
2022-07-21 10:52:11 -0400

    add basic css to updatePointForm

--- src/components/BrowniePoints/UpdatePointsForm.jsx
+++ src/components/BrowniePoints/UpdatePointsForm.jsx
@@ -2,12 +2,14 @@ import React from "react"
 
 const UpdatePointsForm = (props) => {
   return (
-    <div>
-      <h5> {props.math} points {props.math === "Add"? "to": "from"} {props.name}'s points</h5>
-      <form>
-        <input type="number" name="currentPoints"></input>
-        <button> math points</button>
-      </form>
+    <div className="point-form flex-column center">
+      <div>
+        <h5> {props.math} points {props.math === "Add"? "to": "from"} {props.name}'s points</h5>
+        <form>
+          <input type="number" name="currentPoints"></input>
+          <button> math points</button>
+        </form>
+      </div>
     </div>
   )
 }
